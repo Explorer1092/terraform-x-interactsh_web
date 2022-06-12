8d706b25c9e5ccb49d16566bfb69ab8c31dbe329
kevinliu.lk <kevinliu.lk@alibaba-inc.com>
2022-06-12 18:33:44 +0800

    first commit

---
+++ main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    alicloud = {
+      source  = "hashicorp/alicloud"
+      version = "1.170.0"
+    }
+  }
+  required_version = ">=0.13"
+}
+
+variable "name_prefix" {
+  type = string
+}
+
+variable "vswitch_id" {
+  type = string
+}
+
+variable "security_group_id" {
+  type = string
+}
+
+resource "alicloud_eci_container_group" "interactsh" {
+
+  container_group_name = "${var.name_prefix}interactsh-web"
+  cpu                  = 1
+  memory               = 2
+  restart_policy       = "Always"
+  security_group_id    = var.security_group_id
+  vswitch_id           = var.vswitch_id # 交换机
+
+  auto_create_eip = true
+  eip_bandwidth   = 100
+
+  auto_match_image_cache = true
+
+  containers {
+
+    image             = "projectdiscovery/interactsh-web"
+    name              = "${var.name_prefix}interactsh-web"
+    image_pull_policy = "IfNotPresent"
+
+    environment_vars {
+      key   = "PORT"
+      value = 80
+    }
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+
+    commands = ["yarn", "start"]
+  }
+
+}
+
+output "interactsh_public_ip" {
+  value = alicloud_eci_container_group.interactsh.internet_ip
+}
+
+output "interactsh_private_ip" {
+  value = alicloud_eci_container_group.interactsh.intranet_ip
+}
